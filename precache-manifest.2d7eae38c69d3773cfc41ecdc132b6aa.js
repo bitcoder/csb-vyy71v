00ebd455a3df364ec223b1350ce98e079e8c19eb
Code Sandbox <hello@codesandbox.io>
2023-05-10 13:43:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2d7eae38c69d3773cfc41ecdc132b6aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb328f834220d9dd98cc82367731cda1",
+    "url": "https://bitcoder.github.io/csb-vyy71v/index.html"
+  },
+  {
+    "revision": "d66b6b580ec4aa2e3757",
+    "url": "https://bitcoder.github.io/csb-vyy71v/static/css/main.09424398.chunk.css"
+  },
+  {
+    "revision": "ed089fe6969d22cdf1cc",
+    "url": "https://bitcoder.github.io/csb-vyy71v/static/js/2.1bf8dbba.chunk.js"
+  },
+  {
+    "revision": "d66b6b580ec4aa2e3757",
+    "url": "https://bitcoder.github.io/csb-vyy71v/static/js/main.edeb8372.chunk.js"
+  },
+  {
+    "revision": "78f8026941d304f0b4bc",
+    "url": "https://bitcoder.github.io/csb-vyy71v/static/js/runtime~main.bb1acd24.js"
+  }
+]);
